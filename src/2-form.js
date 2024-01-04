48835e0ddbf3ae83aee64dac774fd708b3743411
MyrgorodDenys <117367634+MyrgorodDenys@users.noreply.github.com>
2024-01-04 18:01:30 +0200

    3

---
+++ src/2-form.js
@@ -0,0 +1,39 @@
+const FORM_STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+const formStorageObj = JSON.parse(localStorage.getItem(FORM_STORAGE_KEY)) ?? {
+  email: '',
+  message: '',
+};
+getDataToFillForm(formStorageObj);
+
+form.addEventListener('input', onFormInput);
+
+function onFormInput(e) {
+  formStorageObj[e.target.name] = e.target.value.trim();
+  localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(formStorageObj));
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const eml = form.elements.email.value.trim();
+  const msg = form.elements.message.value.trim();
+
+  if (eml && msg) {
+    localStorage.removeItem(FORM_STORAGE_KEY);
+    e.currentTarget.reset();
+    console.log({ eml, msg });
+  }
+}
+
+function getDataToFillForm(savedDataObj) {
+  Object.keys(savedDataObj).forEach(it => {
+    if (savedDataObj[it]) {
+      form.elements[it].value = savedDataObj[it];
+    }
+  });
+}
